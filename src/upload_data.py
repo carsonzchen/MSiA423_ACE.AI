fe115cbc5c7ef287240a5fc835d54e48e526d863
Ubuntu <ubuntu@ip-172-31-29-22.us-east-2.compute.internal>
2019-05-15 06:00:52 +0000

    make sure upload_data.py runs in ec2

--- src/upload_data.py
+++ src/upload_data.py
@@ -11,14 +11,15 @@ data_permanent_url = 'http://s3.us-east-2.amazonaws.com/nw-carsonchen-acedata/at
 file_name = "atp_data.csv"
 
 ## upload file to s3 project bucket 
-def upload_data(args, filename, output = ''):
-    s3 = boto.client('s3')
-    s3.upload_file(args.bucket, filename, output)
+def upload_data(args):
+    s3 = boto3.client('s3')
+    s3.upload_file(file_name, args.bucket, 'data/{}'.format(file_name))
 
 if __name__ == "__main__":
-	r = requests.get(data_permanent_url)
-	open(file_name, 'wb').write(r.content)
+    r = requests.get(data_permanent_url)
+    open(file_name, 'wb').write(r.content)
     parser = argparse.ArgumentParser(description='Data downloaded as ' + cwd + '\\' + file_name)
     parser.add_argument("--bucket", help="input s3 bucket name")
     args = parser.parse_args()
-    upload_data(args, file_name)
+    upload_data(args)
+    os.remove(file_name)

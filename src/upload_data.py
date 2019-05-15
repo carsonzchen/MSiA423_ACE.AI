78ffcc9b9377dae4e2dd3845605db159fbcc8720
Carson Chen <43358899+carsonzchen@users.noreply.github.com>
2019-05-15 01:08:48 -0500

    Update upload_data.py
    Added comments, removed credential place-holder

--- src/upload_data.py
+++ src/upload_data.py
@@ -2,10 +2,8 @@ import boto3
 import requests
 import argparse
 import os
-cwd = os.getcwd()
 
-#ACCESS_KEY = "My Key"
-#SECRET_KEY = "SK"
+cwd = os.getcwd()
 
 data_permanent_url = 'http://s3.us-east-2.amazonaws.com/nw-carsonchen-acedata/atp_data.csv'
 file_name = "atp_data.csv"
@@ -17,9 +15,9 @@ def upload_data(args):
 
 if __name__ == "__main__":
     r = requests.get(data_permanent_url)
-    open(file_name, 'wb').write(r.content)
+    open(file_name, 'wb').write(r.content) # Temporarily save the data
     parser = argparse.ArgumentParser(description='Data downloaded as ' + cwd + '\\' + file_name)
     parser.add_argument("--bucket", help="input s3 bucket name")
     args = parser.parse_args()
     upload_data(args)
-    os.remove(file_name)
+    os.remove(file_name) # Delete data temporarily saved
